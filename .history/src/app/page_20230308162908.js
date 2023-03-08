ea80a28bd083c9e23360828d92abbca93aaff000
Punit Bathija <punit644@gmail.com>
2023-03-08 16:29:39 +0530

    Handled css not overflowing for mobileview

---
+++ .history/src/app/page_20230308162908.js
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Hero from "./Hero";
+import About from "./About";
+import Feature from "./Feature";
+import Products from "./Products";
+import Reviews from "./Reviews";
+import Footer from "./Footer";
+import ScrollToTopButton from "./ScrollToTopButton";
+
+export default function Home() {
+  return (
+    <div className="overflow-">
+      <Header />
+      <Hero />
+      <About />
+      <Feature />
+      <Products />
+      <Reviews />
+      <Footer />
+      <ScrollToTopButton />
+    </div>
+  );
+}
